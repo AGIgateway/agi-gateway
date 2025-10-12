6db32a8c8f6c81074d6e8f552d0f9f1607775926
Mohammad Munem <munem.mh@gmail.com>
2025-10-12 18:45:13 +1300

    fix: added dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: "build",
-  },
+  base: '/agi-gateway/',
   plugins: [react()],
   resolve: {
     alias: {
